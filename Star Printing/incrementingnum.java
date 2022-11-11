629f1a111233af9400e3f1e29000b01b46995e0f
unza730 <unzasohail819@gmail.com>
2022-11-11 16:44:13 +0500

    j+num printing

--- Star Printing/incrementingnum.java
+++ Star Printing/incrementingnum.java
@@ -25,3 +25,30 @@ public static void main(String args[]) {
 // 333
 // 44
 // 5
+package firstprojectpracrice;
+import java.util.Scanner;
+public class starrprintingpyramid {
+public static void main(String args[]) {
+	int n, num=1;
+	System.out.println("Enter Number");
+
+	Scanner sc = new Scanner(System.in);
+	n= sc.nextInt();
+	for(int i = n; i >= 1; i--) {
+		for(int j = 1; j <= i; j++) 
+		{
+			System.out.print(num+j);
+		}
+		num++;
+		System.out.println();
+	}
+}
+
+}
+// Enter Number
+// 5
+// 23456
+// 3456
+// 456
+// 56
+// 6
