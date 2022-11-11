97c2ff094cc46adae7e786796e82f594034b479b
unza730 <unzasohail819@gmail.com>
2022-11-11 16:42:20 +0500

    reverse j and num increment printing

---
+++ Star Printing/incrementingnum.java
@@ -0,0 +1,27 @@
+package firstprojectpracrice;
+import java.util.Scanner;
+public class starrprintingpyramid {
+public static void main(String args[]) {
+	int n, num=1;
+	System.out.println("Enter Number");
+
+	Scanner sc = new Scanner(System.in);
+	n= sc.nextInt();
+	for(int i = n; i >= 1; i--) {
+		for(int j = 1; j <= i; j++) 
+		{
+			System.out.print(num);
+		}
+		num++;
+		System.out.println();
+	}
+}
+
+}
+// Enter Number
+// 5
+// 11111
+// 2222
+// 333
+// 44
+// 5
--- Star Printing/number(j)printing.java
+++ Star Printing/number(j)printing.java
@@ -24,3 +24,30 @@ public static void main(String args[]) {
 // 123
 // 1234
 // 12345
+package firstprojectpracrice;
+import java.util.Scanner;
+public class starrprintingpyramid {
+public static void main(String args[]) {
+	int n;
+	System.out.println("Enter Number");
+
+	Scanner sc = new Scanner(System.in);
+	n= sc.nextInt();
+	for(int i = n; i >= 1; i--) {
+		for(int j = 1; j <= i; j++) 
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+}
+
+}
+// Enter Number
+// 5
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
