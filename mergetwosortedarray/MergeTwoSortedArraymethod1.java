c11760b6d77fa8669f821007a06808f6b9886c5d
unza730 <unzasohail819@gmail.com>
2023-01-31 00:11:12 +0500

    log add in merge sort

--- mergetwosortedarray/MergeTwoSortedArraymethod1.java
+++ mergetwosortedarray/MergeTwoSortedArraymethod1.java
@@ -12,14 +12,14 @@ public class MergeTwoSortedArraymethod1 {
 		int n2 = arr2.length;
 
 		int arr3[] = new int[n1 + n2];
+
 		mergeArrays(arr1, arr2, n1, n2, arr3);
 
 		System.out.println("Array after merging");
+        
 		for (int i=0; i < n1+n2; i++)
-			System.out.print(arr3[i] + " ");
-			
-	}
-	
+			System.out.print(arr3[i] + " ");		
+	}	
 	public static void mergeArrays(int[] arr1, int[] arr2, int n1, int n2, int[] arr3){
 		int i = 0;
 		int j = 0;
@@ -27,13 +27,18 @@ public class MergeTwoSortedArraymethod1 {
 		
 		// traverse the arr1 and insert its element in arr3
 		while(i < n1){
-		arr3[k++] = arr1[i++];
+          arr3[k] = arr1[i];
+        k++;
+        i++;
+        System.out.println("Printing i: "+ i + "k : " + k);
 		}
-		
 		// now traverse arr2 and insert in arr3
 		while(j < n2){
-		arr3[k++] = arr2[j++];
-		}
+       System.out.println("Printing j:: " + j +"arr2[j]: "+ arr2[j] + " k: "+ k );
+		arr3[k] = arr2[j];
+        k++;
+        j++;
+      }
 		
 		// sort the whole array arr3
 		Arrays.sort(arr3);
