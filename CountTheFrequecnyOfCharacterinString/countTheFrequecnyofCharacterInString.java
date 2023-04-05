f32d6fc4d551353b4bd1686d780483c1056b837b
unza730 <unzasohail819@gmail.com>
2023-04-05 13:56:53 +0500

    vowel Words Character Frequency In String

--- CountTheFrequecnyOfCharacterinString/countTheFrequecnyofCharacterInString.java
+++ CountTheFrequecnyOfCharacterinString/countTheFrequecnyofCharacterInString.java
@@ -11,9 +11,10 @@ public class countTheFrequecnyofCharacterInString {
         System.out.println("The entered string is " + str);
         // Convert the given string into character array
         char str1[] = str.toLowerCase().toCharArray();
-        System.out.println("Str111" + str1[0]);
+        System.out.println("Str111" + str1[0] + " freq-->>  " + freq);
         for (int i = 0; i < str.length(); i++) {
             freq[i] = 1;
+            System.out.println("frq[i]"+ freq[i] + " i " + i);
             for (int j = i + 1; j < str.length(); j++) {
                 if (str1[i] == str1[j]) {
                     freq[i]++;
@@ -43,3 +44,34 @@ public class countTheFrequecnyofCharacterInString {
         }
     }
 }
+
+// public class countTheFrequecnyofCharacterInString {
+//     public static void main(String[] args) {
+//         String str = "Hello World";
+//         int[] freq = new int[256]; // assuming ASCII character set
+//         char[] chars = str.toCharArray();
+// //                 System.out.println(str1[i] + "              " + freq[i]);
+
+//         for (char c : chars) {
+//             freq[c]++;
+// System.out.println("chars:- " + c + "-->" + chars +" freq:->  " + freq[c]);
+
+//         }
+//         for (int i = 0; i < freq.length; i++) {
+//             int max = 0;
+//             char maxChar = 0;
+//             for (int j = 0; j < freq.length; j++) {
+//                 if (freq[j] > max) {
+//                     max = freq[j];
+//                     maxChar = (char)j;
+//                 }
+//             }
+//             if (max > 0) {
+//                 System.out.println(maxChar + " -> " + max);
+//                 freq[maxChar] = 0;
+//             } else {
+//                 break;
+//             }
+//         }
+//     }
+// }
